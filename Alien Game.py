13891549fd208fcce8de5f69f4cd5e4f864ecabd
Matthew DuBois <147000029+duboimd@users.noreply.github.com>
2024-11-21 14:15:44 -0700

    Create Alien Game.py

---
+++ Alien Game.py
@@ -0,0 +1,16 @@
+import random 
+# Welcome to the ALIEN GAME
+# The beginning steps and comments are scaffolded below, but you will need to look at your directions on schoology for the rest
+print("Welcome to the ALIEN GAME!")
+print("You have stolen a UFO to make your way across outer space.")
+print("The aliens want their UFO back and are chasing you down! Survive and out run the Aliens!")
+
+# Create a boolean variable called done and set to False
+
+# Create variables for miles traveled, thirst, and tiredness. Set these to zero
+
+# Create a variable for the distance the aliens have traveled and set it to -20
+
+# Create and set an initial number of drinks in the water bottle - start with 3
+
+# Create a while loop that will keep looping while done is False
